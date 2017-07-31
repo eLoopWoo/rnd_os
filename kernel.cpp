ade835fc05658bc634d3192f14b414983d723f2c
eLoopWoo <eyzenberg.tomer@gmail.com>
2017-07-31 23:20:07 +0300

    added Memory Segments, Global Descriptor Table

--- kernel.cpp
+++ kernel.cpp
@@ -1,8 +1,9 @@
-#include types.h
+#include "types.h"
+#include "gdt.h"
 
 void printf(char* str)
 {
-    static unsigned short* VideoMemory = (unsigned short*)0xb8000;
+    static uint16_t* VideoMemory = (uint16_t*)0xb8000;
 
     for(int i = 0; str[i] != '\0'; ++i)
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
@@ -21,9 +22,11 @@ extern "C" void callConstructors()
 
 
 
-extern "C" void kernelMain(const void* multiboot_structure, unsigned int /*multiboot_magic*/)
+extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot_magic*/)
 {
     printf("Hello World!    Welcome to my operating system");
-
+    
+    GlobalDescriptorTable gdt;
+    
     while(1);
 }
--- makefile
+++ makefile
@@ -1,11 +1,12 @@
 
 # sudo apt-get install g++ binutils libc6-dev-i386
+# sudo apt-get install VirtualBox grub-legacy xorriso
 
 GCCPARAMS = -m32 -fno-use-cxa-atexit -nostdlib -fno-builtin -fno-rtti -fno-exceptions -fno-leading-underscore
 ASPARAMS = --32
 LDPARAMS = -melf_i386
 
-objects = loader.o kernel.o
+objects = loader.o gdt.o kernel.o
 
 
 
