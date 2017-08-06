1bb60161723c6626b20076b3a2a1d4337ddbd78b
eLoopWoo <eyzenberg.tomer@gmail.com>
2017-08-07 00:17:53 +0300

    Added initial gui framework

---
+++ include/common/graphicscontext.h
@@ -0,0 +1,15 @@
+
+#ifndef _RND_OS_COMMON_GRAPHICSCONTEXT
+#define _RND_OS_COMMON_GRAPHICSCONTEXT
+
+#include <drivers/vga.h>
+
+namespace rnd_os
+{
+    namespace common
+    {
+        typedef GraphicsContext VideoGraphicsArray;
+    }
+}
+    
+#endif
