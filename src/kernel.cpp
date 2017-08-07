55b944b06bc1457ac859858072e56676e5f91cb0
eLoopWoo <eyzenberg.tomer@gmail.com>
2017-08-07 03:12:14 +0300

    Added initial gui/window

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -8,6 +8,11 @@
 #include <drivers/mouse.h>
 #include <drivers/vga.h>
 #include <gui/desktop.h>
+#include <gui/window.h>
+
+
+#define GRAPHICSMODE
+
 
 using namespace rnd_os;
 using namespace rnd_os::common;
@@ -126,41 +131,60 @@ extern "C" void kernelMain(const void* multiboot_structure, uint32_t)
 {
     printf("Hello!\n");
 
+
     GlobalDescriptorTable gdt;
     InterruptManager interrupts(0x20, &gdt);
     
     printf("Initializing Hardware, Stage 1\n");
     
+    #ifdef GRAPHICSMODE
+        Desktop desktop(320,200, 0x00,0x00,0xA8);
+    #endif
     
-    Desktop desktop(320, 200, 0x00, 0x00, 0xA8);
-
     DriverManager drvManager;
     
-    //PrintfKeyboardEventHandler kbhandler;
-    //KeyboardDriver keyboard(&interrupts, &kbhandler);
-    KeyboardDriver keyboard(&interrupts, &desktop);
-    drvManager.AddDriver(&keyboard);
+        #ifdef GRAPHICSMODE
+            KeyboardDriver keyboard(&interrupts, &desktop);
+        #else
+            PrintfKeyboardEventHandler kbhandler;
+            KeyboardDriver keyboard(&interrupts, &kbhandler);
+        #endif
+        drvManager.AddDriver(&keyboard);
+        
     
-    //MouseToConsole mousehandler;
-    //MouseDriver mouse(&interrupts, &mousehandler);
-    MouseDriver mouse(&interrupts, &desktop);
-    drvManager.AddDriver(&mouse);
+        #ifdef GRAPHICSMODE
+            MouseDriver mouse(&interrupts, &desktop);
+        #else
+            MouseToConsole mousehandler;
+            MouseDriver mouse(&interrupts, &mousehandler);
+        #endif
+        drvManager.AddDriver(&mouse);
+        
+        PeripheralComponentInterconnectController PCIController;
+        PCIController.SelectDrivers(&drvManager, &interrupts);
 
-    PeripheralComponentInterconnectController PCIController;
-    PCIController.SelectDrivers(&drvManager, &interrupts);
-
-    VideoGraphicsArray vga;
-    
+        VideoGraphicsArray vga;
+        
     printf("Initializing Hardware, Stage 2\n");
-    drvManager.ActivateAll();
-    
+        drvManager.ActivateAll();
+        
     printf("Initializing Hardware, Stage 3\n");
+
+    #ifdef GRAPHICSMODE
+        vga.SetMode(320,200,8);
+        Window win1(&desktop, 10,10,20,20, 0xA8,0x00,0x00);
+        desktop.AddChild(&win1);
+        Window win2(&desktop, 40,15,30,30, 0x00,0xA8,0x00);
+        desktop.AddChild(&win2);
+    #endif
+
+
     interrupts.Activate();
-    
-    vga.SetMode(320,200,8);
     
     while(1)
     {
-        desktop.Draw(&vga);
+        #ifdef GRAPHICSMODE
+            desktop.Draw(&vga);
+        #endif
     }
 }
